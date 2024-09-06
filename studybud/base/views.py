80f8108c60db08dcd39baec9f6c9fb83faf74a24
Sandesh Paudel <sandeshpaudel0618@gmail.com>
2024-09-06 11:13:10 +0545

    Templating Done

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,3 +1,25 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
+rooms=[
+    {
+        'id':1, "name":"Lets Learn Python"},
+        {'id':2, "name":"LDesign With Me"},
+        {'id':3, "name":"FrontEnd Develper"},
+]
+
+
+def home(request):
+    context={'rooms':rooms}
+    return render(request,'base/home.html',context)
+
+def room(request,pk):
+    room=None
+    for i in rooms:
+        if i['id']== int(pk):
+            room=i
+    context={'room':room}
+    return render(request,'base/room.html',context)
