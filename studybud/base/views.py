645f8cb8006f5e3ed31cde3a823a7fc749de132e
Sandesh Paudel <sandeshpaudel0618@gmail.com>
2024-09-07 23:23:02 +0545

    Admin Created with superuser

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,25 +1,32 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import Room
 # Create your views here.
 
 
-rooms=[
-    {
-        'id':1, "name":"Lets Learn Python"},
-        {'id':2, "name":"LDesign With Me"},
-        {'id':3, "name":"FrontEnd Develper"},
-]
+# rooms=[
+#     {
+#         'id':1, "name":"Lets Learn Python"},
+#         {'id':2, "name":"LDesign With Me"},
+#         {'id':3, "name":"FrontEnd Develper"},
+# ]
 
 
 def home(request):
+    rooms=Room.objects.all()
     context={'rooms':rooms}
     return render(request,'base/home.html',context)
 
 def room(request,pk):
-    room=None
-    for i in rooms:
-        if i['id']== int(pk):
-            room=i
+    room=Room.objects.get(id=pk)
+    # room=None
+    # for i in rooms:
+    #     if i['id']== int(pk):
+    #         room=i
     context={'room':room}
     return render(request,'base/room.html',context)
+
+
+def createRoom(request):
+    context={}
+    return render(request,'base/room_form.html',context),
